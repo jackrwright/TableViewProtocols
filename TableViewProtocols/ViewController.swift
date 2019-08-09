0cb7f3ca2f4157fec729bc69dfded0cce0020d02
Jack Wright <jackrwright1@gmail.com>
2019-08-09 09:43:06 -0400

    Initial Commit

---
+++ TableViewProtocols/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewProtocols
+//
+//  Created by Jack Wright on 8/9/19.
+//  Copyright © 2019 Jack Wright. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
